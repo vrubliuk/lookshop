64987a256b035aa3ac8073361083d7729c5ef975
Rubliuk <vrubliuk@plslogistics.com>
2017-01-18 23:52:36 +0200

    Start section 1

--- javascript/js.js
+++ javascript/js.js
@@ -1,4 +1,13 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function ShowCurrencyList() {
+    document.getElementsByClassName("s1_dropdown_content_currency")[0].classList.toggle("show");
+}
 
+function ShowLoginForm() {
+    document.getElementsByClassName("s1_dropdown_content_user")[0].classList.toggle("show");
+}
 
-
-
+function ShowSearchForm() {
+    document.getElementsByClassName("s1_dropdown_content_user")[0].classList.toggle("show");
+}
